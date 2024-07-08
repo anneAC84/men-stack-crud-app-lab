1be4d15c3c768f16750ee3ce936db5446d923c23
Anne <annaconda1984@yahoo.co.uk>
2024-07-08 09:16:40 +0100

    Homework

--- server.js
+++ server.js
@@ -2,16 +2,19 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const morgan = require('morgan')
 const ejs = require('ejs')
 const Films = require('./models/films.js')
+const methodOverride = require("method-override");
+const morgan = require('morgan')
 
 //constants
 const app = express()
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.use(morgan('dev'))
+
 
 //routes
 
@@ -35,18 +38,47 @@ app.post('/films', async (req, res) => {
     res.redirect("/films");
   });
 
-  // GET /films
+  // GET /films index
   app.get("/films", async (req, res) => {
     const allFilms = await Films.find();
     res.render("films/index.ejs", { films: allFilms });
   });
   
   
+  //show films
+  app.get("/films/:filmsId", async (req, res) => {
+    const foundFilms = await Films.findById(req.params.filmsId);
+    res.render('films/show.ejs', { films: foundFilms});
+});
+
+app.delete("/films/:filmsId", async (req, res) => {
+    await Films.findByIdAndDelete(req.params.filmsId)
+    
+    res.redirect('/films');
+  });
   
+  
+  // GET localhost:3000/fruits/:fruitId/edit
+app.get("/films/:filmsId/edit", async (req, res) => {
+    const foundFilms = await Films.findById(req.params.filmsId);
+     res.render('films/edit.ejs',{ 
+        films: foundFilms,
+  });
+});
   
 
+// server.js
 
+app.put("/films/:filmsId", async (req, res) => {
+    
+    // Update the fruit in the database
+    await Films.findByIdAndUpdate(req.params.filmsId, req.body);
   
+    // Redirect to the fruit's show page to see the updates
+    res.redirect(`/films/${req.params.filmsId}`);
+  });
+  
+
 //server connections
 const connect = async () =>{
     try {
